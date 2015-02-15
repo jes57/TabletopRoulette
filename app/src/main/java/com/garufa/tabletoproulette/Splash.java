11135dbf383f91b5c76e4e4bcd257f7de7b49929
jes57 <jasonstephens1084@gmail.com>
2015-02-15 17:15:03 -0500

    Set up the splash so it displays the log and then goes to mainActivity

---
+++ app/src/main/java/com/garufa/tabletoproulette/Splash.java
@@ -0,0 +1,34 @@
+package com.garufa.tabletoproulette;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+/**
+ * Created by Jason on 2/15/2015.
+ */
+public class Splash extends Activity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000; // 3 Seconds
+    String TAG = "Splash";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        Log.i(TAG, "Inside Splash");
+
+        final Intent menuIntent = new Intent(Splash.this, MainActivity.class);
+
+        // Keep this open until the time has expired and return to the quiz
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(menuIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
